d363e5ea184d65560184f452687b544d74899828
wb-lj789114 <wb-lj789114@alibaba-inc.com>
2021-08-12 09:41:25 +0800

    feat: 更新 tesButton.jsx 组件的测试用例

--- test/testButton.tsx
+++ test/testButton.tsx
@@ -1,6 +1,8 @@
-/* eslint-disable no-undef */
+/* eslint-disable no-undef,max-lines-per-function */
 import React from 'react';
-import '../dist/athene.min.css';
+import '../components/button/style';
+import '../components/wing-blank/style';
+import '../components/white-space/style';
 
 // const WingBlank = require("../lib/wing-blank").default;
 // const { WingBlankTypeEnum } = require("../lib/wing-blank");
@@ -10,9 +12,13 @@ import '../dist/athene.min.css';
 
 // const { WingBlank, WingBlankTypeEnum, WhiteSpace, Button, ButtonTypeEnum, ButtonStyleEnum } = require('../dist/athene');
 
-import WingBlank, { WingBlankTypeEnum } from '../es/wing-blank';
-import WhiteSpace from '../es/white-space';
-import Button, { ButtonTypeEnum, ButtonStyleEnum } from '../es/button';
+// import WingBlank, { WingBlankTypeEnum } from '../es/wing-blank';
+// import WhiteSpace from '../es/white-space';
+// import Button, { ButtonTypeEnum, ButtonStyleEnum } from '../es/button';
+
+import WingBlank, { WingBlankTypeEnum } from '../components/wing-blank';
+import WhiteSpace from '../components/white-space';
+import Button, { ButtonTypeEnum, ButtonStyleEnum } from '../components/button';
 
 const TestButton: React.FC = () => {
 	return (
@@ -23,16 +29,152 @@ const TestButton: React.FC = () => {
 			</WingBlank>
 			<WhiteSpace />
 			<WingBlank type={WingBlankTypeEnum.MIDDLE}>
-				<Button type={ButtonTypeEnum.PRIMARY}>primary 按钮</Button>
+				<Button
+					type={ButtonTypeEnum.PRIMARY}
+					waterRipple={{
+						color: '#FFF',
+						opacity: 0.5,
+					}}
+				>
+					primary 按钮
+				</Button>
 			</WingBlank>
 			<WhiteSpace />
 			<WingBlank type={WingBlankTypeEnum.SMALL}>
-				<Button type={ButtonTypeEnum.DANGER}>danger 按钮</Button>
-			</WingBlank>
-			<WhiteSpace />
-			<WingBlank type={WingBlankTypeEnum.SMALL}>
-				<Button type={ButtonTypeEnum.DANGER} buttonStyle={ButtonStyleEnum.GHOST}>
+				<Button
+					type={ButtonTypeEnum.DANGER}
+					waterRipple={{
+						color: '#FFF',
+						opacity: 0.5,
+					}}
+				>
 					danger 按钮
+				</Button>
+			</WingBlank>
+			<WhiteSpace />
+			<WingBlank type={WingBlankTypeEnum.SMALL}>
+				<Button
+					type={ButtonTypeEnum.WARNING}
+					waterRipple={{
+						color: '#FFF',
+						opacity: 0.5,
+					}}
+				>
+					warning 按钮
+				</Button>
+			</WingBlank>
+			<WhiteSpace />
+			<WingBlank type={WingBlankTypeEnum.SMALL}>
+				<Button
+					buttonStyle={ButtonStyleEnum.TEXT}
+					waterRipple={{
+						color: '#000',
+						opacity: 0.5,
+					}}
+				>
+					text 按钮
+				</Button>
+			</WingBlank>
+			<WhiteSpace />
+			<WingBlank type={WingBlankTypeEnum.SMALL}>
+				<Button
+					buttonStyle={ButtonStyleEnum.TEXT}
+					type={ButtonTypeEnum.PRIMARY}
+					waterRipple={{
+						color: '#1976d2',
+						opacity: 0.5,
+					}}
+				>
+					text 按钮
+				</Button>
+			</WingBlank>
+			<WhiteSpace />
+			<WingBlank type={WingBlankTypeEnum.SMALL}>
+				<Button
+					buttonStyle={ButtonStyleEnum.TEXT}
+					type={ButtonTypeEnum.WARNING}
+					waterRipple={{
+						color: '#ffc800',
+						opacity: 0.5,
+					}}
+				>
+					text 按钮
+				</Button>
+			</WingBlank>
+			<WhiteSpace />
+			<WingBlank type={WingBlankTypeEnum.SMALL}>
+				<Button
+					buttonStyle={ButtonStyleEnum.TEXT}
+					type={ButtonTypeEnum.DANGER}
+					waterRipple={{
+						color: '#dc014e',
+						opacity: 0.5,
+					}}
+				>
+					text 按钮
+				</Button>
+			</WingBlank>
+			<WhiteSpace />
+			<WingBlank type={WingBlankTypeEnum.SMALL}>
+				<Button
+					buttonStyle={ButtonStyleEnum.LINK}
+					waterRipple={{
+						color: '#1976d2',
+						opacity: 0.5,
+					}}
+				>
+					link 按钮
+				</Button>
+			</WingBlank>
+			<WhiteSpace />
+			<WingBlank type={WingBlankTypeEnum.SMALL}>
+				<Button
+					buttonStyle={ButtonStyleEnum.GHOST}
+					waterRipple={{
+						color: '#000000de',
+						opacity: 0.5,
+					}}
+				>
+					ghost 按钮
+				</Button>
+			</WingBlank>
+			<WhiteSpace />
+			<WingBlank type={WingBlankTypeEnum.SMALL}>
+				<Button
+					buttonStyle={ButtonStyleEnum.GHOST}
+					type={ButtonTypeEnum.PRIMARY}
+					waterRipple={{
+						color: '#1976d2',
+						opacity: 0.5,
+					}}
+				>
+					ghost 按钮
+				</Button>
+			</WingBlank>
+			<WhiteSpace />
+			<WingBlank type={WingBlankTypeEnum.SMALL}>
+				<Button
+					buttonStyle={ButtonStyleEnum.GHOST}
+					type={ButtonTypeEnum.WARNING}
+					waterRipple={{
+						color: '#ffc800',
+						opacity: 0.5,
+					}}
+				>
+					ghost 按钮
+				</Button>
+			</WingBlank>
+			<WhiteSpace />
+			<WingBlank type={WingBlankTypeEnum.SMALL}>
+				<Button
+					buttonStyle={ButtonStyleEnum.GHOST}
+					type={ButtonTypeEnum.DANGER}
+					waterRipple={{
+						color: '#dc004e',
+						opacity: 0.5,
+					}}
+				>
+					ghost 按钮
 				</Button>
 			</WingBlank>
 		</React.Fragment>
