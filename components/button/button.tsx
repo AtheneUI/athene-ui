6a4ad77a2e3a77497d0049a37884f4bc30d6166c
wb-lj789114 <wb-lj789114@alibaba-inc.com>
2021-08-12 19:03:54 +0800

    feat: update button component

--- components/button/button.tsx
+++ components/button/button.tsx
@@ -1,75 +1,140 @@
-/* eslint-disable react/prop-types */
-import React, { useEffect, useRef, CSSProperties, ReactNode } from 'react';
-import { btnDelay } from './types';
+/* eslint-disable react/prop-types,max-lines-per-function */
+import React, { useEffect, useState, useRef, CSSProperties } from 'react';
 import classNames from 'classnames';
-import { addWaterRipple } from '../_utils';
-import { ButtonTypeEnum, ButtonSizeEnum, ButtonIconPositionEnum, ButtonNativeTypeEnum, ButtonShapeEnum, ButtonStyleEnum } from './enum';
-
-interface IWaterRipple {
-	color?: string;
-	speed?: number;
-	opacity?: number;
-}
-
-interface IButtonProps {
-	onClick?: React.MouseEventHandler<HTMLElement>;
-	iconPosition?: ButtonIconPositionEnum;
-	nativeType?: ButtonNativeTypeEnum;
-	loading?: boolean | btnDelay;
-	style?: CSSProperties;
-	to?: string;
-	shape?: ButtonShapeEnum;
-	type?: ButtonTypeEnum;
-	buttonStyle?: ButtonStyleEnum;
-	size?: ButtonSizeEnum;
-	disabled?: boolean;
-	replace?: boolean;
-	icon?: ReactNode;
-	href?: string;
-	children?: ReactNode;
-	disableElevation?: boolean;
-	waterRipple?: boolean | IWaterRipple;
-}
+import { addWaterRipple, getType, isObject } from '../_utils';
+import type { IWaterRipple, ISrc, IButtonProps } from './types';
+import {
+	buttonStyleColors,
+	buttonStyleGhostBorderColors,
+	buttonTypeColors,
+	buttonSize,
+	buttonStyleDisabledColor,
+	buttonStyleDisabledBorderColor,
+	buttonStyleDisabledBorderWidth,
+	buttonStyleDisabledBorderType,
+	buttonStyleDisabledBgColor,
+	buttonTypeDisabledColor,
+	buttonTypeDisabledBgColor,
+	buttonTypeDefaultColor,
+	buttonTypeColor,
+	jumpMode,
+} from './constant';
+import { ButtonTypeEnum, ButtonSizeEnum, ButtonNativeTypeEnum, ButtonStyleEnum, ButtonJumpEnum } from './enum';
 
 // deal add water ripple with button
 const dealWaterRipple = (button: HTMLButtonElement, waterRipple?: boolean | IWaterRipple) => {
-	if (typeof waterRipple === 'boolean' || waterRipple === undefined) {
+	if (getType(waterRipple) === 'boolean' || waterRipple === undefined) {
 		waterRipple === undefined || waterRipple === true ? addWaterRipple(button) : null;
 		return;
 	}
-	if (typeof waterRipple === 'object' && waterRipple !== null) {
+	if (isObject(waterRipple)) {
 		addWaterRipple(button, waterRipple as IWaterRipple);
 		return;
 	}
 };
 
-// deal button's style
-const dealButtonStyle = (type?: ButtonTypeEnum, buttonStyle?: ButtonStyleEnum) => {
-	let buttonClassName = '';
-	const _type = type ? type : 'default';
-	if (buttonStyle) {
-		buttonClassName = `athene-button-${_type}-${buttonStyle}`;
-	} else {
-		buttonClassName = `athene-button-${_type}`;
-	}
-	return buttonClassName;
+// different page jump action's map
+const pageJumpMap = {
+	[ButtonJumpEnum.HREF]: (src: ISrc) => {
+		// use alink jump
+		console.log(src);
+	},
+	[ButtonJumpEnum.REPLACE]: (src: ISrc) => {
+		// use browser native api jump
+		if (window.location.replace) window.location.replace(src.path);
+	},
+	[ButtonJumpEnum.TO]: (src: ISrc) => {
+		// use route jump
+		console.log(src);
+	},
 };
 
 const Button: React.FC<IButtonProps> = props => {
-	const { type, buttonStyle, disableElevation = false, waterRipple } = props || {};
+	const {
+		type,
+		buttonStyle,
+		disableElevation = false,
+		waterRipple,
+		nativeType = ButtonNativeTypeEnum.BUTTON,
+		disabled = false,
+		style = {},
+		size = ButtonSizeEnum.NORMAL,
+		src = { type: ButtonJumpEnum.HREF, path: '' },
+	} = props || {};
 	const buttonRef = useRef<HTMLButtonElement>(null);
-	useEffect((): void => {
-		const button = buttonRef && buttonRef.current;
-		if (button) {
-			dealWaterRipple(button, waterRipple);
-		}
+	const [innerStyle, setInnerStyle] = useState<CSSProperties>({});
+	const [visible, setVisible] = useState(false);
+
+	useEffect(() => {
+		dealButtonStyle(type, buttonStyle, disabled);
+		// component visible
+		setVisible(true);
 	}, []);
 
-	return (
-		<button ref={buttonRef} className={classNames('athene-button', dealButtonStyle(type, buttonStyle), !disableElevation ? 'button-elevation' : '')}>
+	useEffect((): void => {
+		if (buttonRef.current && !disabled) {
+			// add water ripple dynamic effect
+			dealWaterRipple(buttonRef.current, waterRipple);
+		}
+	}, [buttonRef, visible]);
+
+	// button style's map
+	const buttonStyleMap = {
+		[ButtonStyleEnum.TEXT]: (disabled: boolean, type: string) => {
+			changeStyleHandle(disabled ? buttonStyleDisabledColor : buttonStyleColors[type as ButtonTypeEnum], buttonStyleDisabledBgColor);
+		},
+		[ButtonStyleEnum.LINK]: (disabled: boolean) => {
+			changeStyleHandle(disabled ? buttonStyleDisabledColor : buttonStyleColors[ButtonTypeEnum.PRIMARY], buttonStyleDisabledBgColor);
+		},
+		[ButtonStyleEnum.GHOST]: (disabled: boolean, type: string) => {
+			changeStyleHandle(disabled ? buttonStyleDisabledColor : buttonStyleColors[type as ButtonTypeEnum], buttonStyleDisabledBgColor, {
+				border: `${buttonStyleDisabledBorderWidth} ${buttonStyleDisabledBorderType} ${disabled ? buttonStyleDisabledBorderColor : buttonStyleGhostBorderColors[type as ButtonTypeEnum]}`,
+			});
+		},
+	};
+
+	// deal button's style
+	const dealButtonStyle = (type?: ButtonTypeEnum, buttonStyle?: ButtonStyleEnum, disabled?: boolean) => {
+		const _type: ButtonTypeEnum = type ? type : ButtonTypeEnum.DEFAULT;
+		if (buttonStyle) {
+			buttonStyleMap[buttonStyle](disabled as boolean, _type);
+		} else {
+			disabled
+				? changeStyleHandle(buttonTypeDisabledColor, buttonTypeDisabledBgColor, { boxShadow: 'none' })
+				: changeStyleHandle(_type === ButtonTypeEnum.DEFAULT ? buttonTypeDefaultColor : buttonTypeColor, buttonTypeColors[_type]);
+		}
+	};
+
+	// change button style's handler
+	const changeStyleHandle = (color: string, background: string, options: CSSProperties = {}) => {
+		setInnerStyle({
+			color,
+			background,
+			...options,
+		});
+	};
+
+	// button click's callback
+	const onClickButton = () => {
+		// execute user custom click event's callback
+		props.onClick && props.onClick();
+		// check and deal with current params for [href、replace、to]
+		if (src.path && jumpMode.includes(src.type as string)) {
+			pageJumpMap[src.type as ButtonJumpEnum](src);
+		}
+	};
+
+	return visible ? (
+		<button
+			ref={buttonRef}
+			className={classNames('athene-button', buttonStyle ? 'athene-button-type-buttonStyle' : '', !disableElevation ? 'button-elevation' : '')}
+			type={nativeType}
+			style={{ ...innerStyle, ...style, ...buttonSize[size] }}
+			onClick={onClickButton}
+		>
 			{props.children}
 		</button>
-	);
+	) : null;
 };
 
 export default Button;
