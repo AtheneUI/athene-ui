7dbb0c4f996fa1af70e106d8aaae6357b795e3a6
wb-lj789114 <wb-lj789114@alibaba-inc.com>
2021-08-12 09:39:28 +0800

    feat: 更改 button 组件内部结构与样式，使其支持添加水波纹特效

--- components/button/button.tsx
+++ components/button/button.tsx
@@ -1,8 +1,15 @@
 /* eslint-disable react/prop-types */
-import React, { CSSProperties, ReactNode } from 'react';
+import React, { useEffect, useRef, CSSProperties, ReactNode } from 'react';
 import { btnDelay } from './types';
 import classNames from 'classnames';
+import { addWaterRipple } from '../_utils';
 import { ButtonTypeEnum, ButtonSizeEnum, ButtonIconPositionEnum, ButtonNativeTypeEnum, ButtonShapeEnum, ButtonStyleEnum } from './enum';
+
+interface IWaterRipple {
+	color?: string;
+	speed?: number;
+	opacity?: number;
+}
 
 interface IButtonProps {
 	onClick?: React.MouseEventHandler<HTMLElement>;
@@ -20,18 +27,49 @@ interface IButtonProps {
 	icon?: ReactNode;
 	href?: string;
 	children?: ReactNode;
+	disableElevation?: boolean;
+	waterRipple?: boolean | IWaterRipple;
 }
 
-const buttonTypes: Array<string> = ['primary', 'danger', 'default'];
+// deal add water ripple with button
+const dealWaterRipple = (button: HTMLButtonElement, waterRipple?: boolean | IWaterRipple) => {
+	if (typeof waterRipple === 'boolean' || waterRipple === undefined) {
+		waterRipple === undefined || waterRipple === true ? addWaterRipple(button) : null;
+		return;
+	}
+	if (typeof waterRipple === 'object' && waterRipple !== null) {
+		addWaterRipple(button, waterRipple as IWaterRipple);
+		return;
+	}
+};
+
+// deal button's style
+const dealButtonStyle = (type?: ButtonTypeEnum, buttonStyle?: ButtonStyleEnum) => {
+	let buttonClassName = '';
+	const _type = type ? type : 'default';
+	if (buttonStyle) {
+		buttonClassName = `athene-button-${_type}-${buttonStyle}`;
+	} else {
+		buttonClassName = `athene-button-${_type}`;
+	}
+	return buttonClassName;
+};
+
 const Button: React.FC<IButtonProps> = props => {
-	const { type } = props;
+	const { type, buttonStyle, disableElevation = false, waterRipple } = props || {};
+	const buttonRef = useRef<HTMLButtonElement>(null);
+	useEffect((): void => {
+		const button = buttonRef && buttonRef.current;
+		if (button) {
+			dealWaterRipple(button, waterRipple);
+		}
+	}, []);
 
-	const getButtonStyle = (): string => {
-		const _type = type?.toLowerCase();
-		return _type && buttonTypes.includes(_type) ? `athene-button-${_type}` : 'athene-button-default';
-	};
-
-	return <button className={classNames('athene-button-base-style', 'athene-button', getButtonStyle())}>{props.children}</button>;
+	return (
+		<button ref={buttonRef} className={classNames('athene-button', dealButtonStyle(type, buttonStyle), !disableElevation ? 'button-elevation' : '')}>
+			{props.children}
+		</button>
+	);
 };
 
 export default Button;
