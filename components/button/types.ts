9d28363d39a79ff96a6425d514cfcacb76e38e7c
wb-lj789114 <wb-lj789114@alibaba-inc.com>
2021-08-12 19:06:33 +0800

    feat: add some types

--- components/button/types.ts
+++ components/button/types.ts
@@ -1,3 +1,35 @@
+import React, { CSSProperties, ReactNode } from 'react';
+import { ButtonTypeEnum, ButtonSizeEnum, ButtonNativeTypeEnum, ButtonShapeEnum, ButtonStyleEnum, ButtonJumpEnum } from './enum';
+
 export type btnDelay = {
 	delay: number;
 };
+
+export interface IWaterRipple {
+	color?: string;
+	speed?: number;
+	opacity?: number;
+}
+
+export interface ISrc {
+	type?: ButtonJumpEnum;
+	path: string;
+}
+
+export interface IButtonProps {
+	waterRipple?: boolean | IWaterRipple;
+	nativeType?: ButtonNativeTypeEnum;
+	buttonStyle?: ButtonStyleEnum;
+	loading?: boolean | btnDelay;
+	startIcon?: React.ReactNode;
+	disableElevation?: boolean;
+	endIcon?: React.ReactNode;
+	shape?: ButtonShapeEnum;
+	style?: CSSProperties;
+	type?: ButtonTypeEnum;
+	size?: ButtonSizeEnum;
+	onClick?: () => void;
+	children?: ReactNode;
+	disabled?: boolean;
+	src?: ISrc;
+}
